e77a7a52719edcc06f7ce75b8a49164d392460c2
shelleywestover <shelleywestover@gmail.com>
2014-02-12 23:41:34 -0500

    commenting all over the place. Completely failed to implement having video play through to next waypoint despite hours apon hours of work on it

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ function updateWaypoints() {
 	fractionScrolled = scrolled / scrollTotal;
 
 	// 0 <= fractionScrolled <= 1, so *10 gives us 10; Math.floor rounds down
-	var whichWaypoint = Math.max(0, Math.floor(fractionScrolled * 10) - 1);
+	var whichWaypoint
+	 = Math.max(0, Math.floor(fractionScrolled * 10) - 1);
 
 	for (i = 0; i < 10; i++) {
 		// Notice we constructed our li#id names to make this easy
@@ -35,6 +36,7 @@ function updateWaypoints() {
 	}
 
 	// Seek to the proportional time of the 38s clip of Bey's "Countdown"
+	// I am pretty sure this is the line that advances your video
 	document.getElementById('Countdown').currentTime = fractionScrolled * 136.0;
 
 	// adds text to top left aside when scrolled reaches 200
@@ -103,6 +105,9 @@ function updateWaypoints() {
 
 function waypointClickHandler(e) {
 	console.log('click');
+	//var current = scrolled
+    //console.log("current = " + current);
+
 	for (i = 0; i < waypoints.length; i++) {
 		if (waypoints[i] === this) {
 			scrolled = (i+1)*100;
@@ -110,6 +115,33 @@ function waypointClickHandler(e) {
 			console.log(scrolled);
 		}
 	}
+   /* var next = this //Math.ceil(((scrolled % scrollTotal) + 100)/100) * 100;
+	console.log("next = " + next);
+	
+	//add delay to for look (http://stackoverflow.com/questions/10058753/how-to-create-pause-or-delay-in-for-loop)
+	if (next > current) {
+		for (var i = current; i < next; i++) {
+			console.log("before")
+			scrolled += 1;
+			window.setTimeout(function(){
+				updateWaypoints();
+				}, 1000);
+			console.log("after")    
+			console.log("scrolled =" + scrolled)
+		}
+	}
+	if (next < current) {
+		for (var i = next; i < current; i++) {
+				console.log("before")
+				scrolled -= 1;
+				window.setTimeout(function(){
+					updateWaypoints();
+					}, 1000);
+				console.log("after")    
+				console.log("scrolled =" + scrolled)
+			}
+	} */
+		
 }
 
 
@@ -129,10 +161,24 @@ var next = document.getElementById('next-triangle');
 
 next.onclick = function() {
     if (scrolled == 0) {
-    	scrolled += 100
+    	scrolled += 100;
     }
-	scrolled = Math.ceil(((scrolled % scrollTotal) + 100)/100) * 100;
-	console.log("scrolled = " + scrolled);
+    scrolled = Math.ceil(((scrolled % scrollTotal) + 100)/100) * 100;
+	
+	/* A colleague and I were experimenting with different ways of advancing the video. We weren't successful, but I am keeping the below code and comments as a reference so that we can try again at a later date.
+
+	// this is how i would try to implement this, put the following in a loop that keeps updating
+	// gets the video element, checks where the playback is at in Milliseconds (converted to frames by multiplying).  Checks if the current frame is == to the frame at which you should stop. (stoppingPoint is made up, psuedocode by me)
+	// for example, for chapter 1->2
+	// first play the video
+	document.getElementById('Countdown').play(); //from chapter 1)
+	// now in a loop, check if we're past the next chapter (13.6 secs per chapter)
+	if (document.getElementById('Countdown').currentTime  * 1000 >= 13.6 * nextChapter) // you're checking to see if the video has played past the chapter you want to stop at
+	{
+		// if we are past or at the next chapter, pause the video
+		document.getElementById('Countdown').pause(); // double check actual control
+	} */
+
 	updateWaypoints();
-	console.log(scrolled);  			
+	console.log("scrolled = " + scrolled)
 }
